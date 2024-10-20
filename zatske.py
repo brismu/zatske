3878f74de170d21c0f8b07f3be66f5ba1df703ee
Corbin <cds@corbinsimpson.com>
2024-10-20 12:37:35 -0700

    Flesh out a complete example.

--- zatske.py
+++ zatske.py
@@ -11,6 +11,7 @@ SCHEMA_PATH = "@SCHEMA@"
 @click.group()
 @click.pass_context
 def cli(ctx):
+    print("Loading schema:", SCHEMA_PATH)
     with open(SCHEMA_PATH, "rb") as handle:
         ctx.obj["SCHEMA"] = json.load(handle)
 
@@ -18,8 +19,12 @@ def cli(ctx):
 @click.argument("olog", type=click.File("rb"))
 @click.pass_context
 def summarize(ctx, olog):
-    validate(ctx.obj["SCHEMA"], json.load(olog))
+    olog = json.load(olog)
+    validate(ctx.obj["SCHEMA"], olog)
     print("Yep, it's an olog!")
+    print("Types:", len(olog["types"]))
+    print("Aspects:", *list(olog["aspects"].keys()))
+    print("Facts:", len(olog["facts"]))
 
 register_repl(cli)
 cli(obj={})
